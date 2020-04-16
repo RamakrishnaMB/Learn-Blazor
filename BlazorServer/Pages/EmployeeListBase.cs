4bc778c46257e0bd7b371cef5f150396db974915
Ramakrishna <ramakrishnamb@gmail.com>
2020-04-16 16:48:33 +0530

    Blazor initial commit - Part 21 - Call REST API from ASP NET Core Blazor

---
+++ BlazorServer/Pages/EmployeeListBase.cs
@@ -0,0 +1,81 @@
+﻿using BlazorServer.Services;
+using EmployeeManagement.Models;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BlazorServer.Pages
+{
+    public class EmployeeListBase : ComponentBase
+    {
+        //so it is razor, we cannot intitialize service using constructor. so I need to inject the service.
+        [Inject]
+        public IEmployeeService EmployeeService { get; set; }
+
+        public IEnumerable<Employee> Employeess { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            // await Task.Run(LoadEmployees);
+            Employeess =(await EmployeeService.GetEmployees()).ToList();
+        }
+
+
+        //private void LoadEmployees()
+        //{
+        //    Thread.Sleep(2000);
+        //    Employee e1 = new Employee
+        //    {
+        //        EmployeeId = 1,
+        //        FirstName = "John",
+        //        LastName = "Hastings",
+        //        Email = "David@pragimtech.com",
+        //        DateOfBrith = new DateTime(1980, 10, 5),
+        //        Gender = Gender.Male,
+        //        DepartmentId = 1,
+        //        PhotoPath = "images/john.png"
+        //    };
+
+        //    Employee e2 = new Employee
+        //    {
+        //        EmployeeId = 2,
+        //        FirstName = "Sam",
+        //        LastName = "Galloway",
+        //        Email = "Sam@pragimtech.com",
+        //        DateOfBrith = new DateTime(1981, 12, 22),
+        //        Gender = Gender.Male,
+        //        DepartmentId = 2,
+        //        PhotoPath = "images/sam.jpg"
+        //    };
+
+        //    Employee e3 = new Employee
+        //    {
+        //        EmployeeId = 3,
+        //        FirstName = "Mary",
+        //        LastName = "Smith",
+        //        Email = "mary@pragimtech.com",
+        //        DateOfBrith = new DateTime(1979, 11, 11),
+        //        Gender = Gender.Female,
+        //        DepartmentId = 3,
+        //        PhotoPath = "images/mary.png"
+        //    };
+
+        //    Employee e4 = new Employee
+        //    {
+        //        EmployeeId = 3,
+        //        FirstName = "Sara",
+        //        LastName = "Longway",
+        //        Email = "sara@pragimtech.com",
+        //        DateOfBrith = new DateTime(1982, 9, 23),
+        //        Gender = Gender.Female,
+        //        DepartmentId = 4,
+        //        PhotoPath = "images/sara.png"
+        //    };
+
+        //    Employeess = new List<Employee> { e1, e2, e3, e4 };
+        //}
+    }
+}
---
+++ EmployeeManagement.Api/Controllers/EmployeeController.cs
@@ -0,0 +1,161 @@
+﻿using EmployeeManagement.Api.Models;
+using EmployeeManagement.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+
+namespace EmployeeManagement.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        //from hardcoded service
+        private readonly IEmployeeRepository employeeRepository;
+
+        public EmployeeController(IEmployeeRepository employeeRepository)
+        {
+            this.employeeRepository = employeeRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetEmployes()
+        {
+            try
+            {
+                return Ok(await employeeRepository.GetEmployees());
+            }
+            catch (System.Exception)
+            {
+
+                throw;
+            }
+
+        }
+
+        [HttpGet("{Id:int}")]
+        public async Task<ActionResult<Employee>> GetEmployeByID(int Id)
+        {
+            try
+            {
+                var result = await employeeRepository.GetEmployee(Id);
+
+                if (result == null)
+                {
+                    return NotFound();
+                }
+
+                return result;
+            }
+            catch (System.Exception)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error retrieving data from database");
+            }
+        }
+
+
+        [HttpPost]
+        public async Task<ActionResult<Employee>> CreateEmployee(Employee employee)
+        {
+            try
+            {
+                if (employee == null)
+                {
+                    return BadRequest();
+                }
+                else
+                {
+                    var empEmail = employeeRepository.GetEmployeeByEmail(employee.Email);
+                    if (empEmail != null)
+                    {
+                        ModelState.AddModelError("email", "Employee email already in user");
+                        return BadRequest(ModelState);
+                    }
+                    var empCreatedObj = await employeeRepository.AddEmployee(employee);
+                    return CreatedAtAction(nameof(GetEmployeByID), new { Id = empCreatedObj.EmployeeId }, empCreatedObj);
+                }
+
+            }
+            catch (System.Exception)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error retrieving data from database");
+            }
+
+        }
+
+        [HttpPut("{id:int}")]
+        public async Task<ActionResult<Employee>> UpdateEmployee(int id, Employee employee)
+        {
+            try
+            {
+                if (id != employee.EmployeeId)
+                {
+                    return BadRequest("Employe ID Mismatch");
+                }
+                var empToupdate = await employeeRepository.GetEmployee(id);
+
+                if (empToupdate == null)
+                {
+                    return NotFound($"Employee with Id ={id} not found.");
+                }
+                return await employeeRepository.UpdateEmployee(employee);
+            }
+            catch (System.Exception)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error Updating data from database");
+            }
+
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult<Employee>> DeleteEmployee(int id)
+        {
+            try
+            {
+                var emp = await employeeRepository.GetEmployee(id);
+
+                if (emp == null)
+                {
+                    return NotFound($"Employee with Id ={id} not found");
+                }
+                return await employeeRepository.DeleteEmployee(id);
+            }
+            catch (System.Exception)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error Updating data from database");
+            }
+        }
+
+        [HttpGet("search")]
+        public async Task<ActionResult<IEnumerable<Employee>>> Search(string name, Gender? gender)
+        {
+            try
+            {
+                var employees = await employeeRepository.Search(name, gender);
+
+                if (employees != null)
+                {
+                    return Ok(employees);
+                }
+                return NotFound();
+            }
+            catch (System.Exception)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error Updating data from database");
+            }
+        }
+
+    }
+
+
+
+
+}
