068b3024430c1b9b810d144042d39a0876131b9f
Ramakrishna <ramakrishnamb@gmail.com>
2020-04-27 18:30:56 +0530

    Part 29 - Building a blazor form using EditForm to edit an employee.

---
+++ BlazorServer/Pages/EditEmployeeBase.cs
@@ -0,0 +1,28 @@
+﻿using BlazorServer.Services;
+using EmployeeManagement.Models;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorServer.Pages
+{
+    public class EditEmployeeBase : ComponentBase
+    {
+        public int id { get; set; }
+
+        [Inject]
+        public IEmployeeService EmployeeService { get; set; }
+
+        public Employee Employee { get; set; } = new Employee();
+
+        [Parameter]
+        public string Id { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            Employee = await EmployeeService.GetEmployee(int.Parse(Id));
+        }
+    }
+}
